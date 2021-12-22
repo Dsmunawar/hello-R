a4029e5afc804769141e1a436e7055be45ec6037
Dsmunawar <munawar00567@gmail.com>
2021-12-22 21:11:29 +0500

    Few lines added

--- testint.r
+++ testint.r
@@ -1,2 +1,7 @@
 # This is from git
 print("Hello from github")
+name = "munawar"
+version = "Rstudio in writing "
+print(name)
+print("These line has been written in Rstudio")
+print(" munawar hussain is expert trainer")
